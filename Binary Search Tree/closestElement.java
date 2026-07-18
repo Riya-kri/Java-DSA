0246c35769cbffb0e36aab067f5ddb1c8ffaf705
Riya Kumari <riyamuskan12345@gmail.com>
2026-07-18 21:36:32 +0530

    Add files via upload

---
+++ Binary Search Tree/closestElement.java
@@ -0,0 +1,30 @@
+package apnaCollege;
+import apnaCollege.search.Node;
+public class closestElement 
+{
+	static int closest = Integer.MAX_VALUE;
+	public static int difference(Node root, int k)
+	{
+		if(root == null)
+			return closest;
+		
+		if ( Math.abs(root.data - k) < Math.abs(closest - k)) {
+		     closest = root.data;
+	    }
+		difference(root.left, k);
+		difference(root.right, k);
+
+		return closest;
+	}
+	public static void main(String[] args) {
+        // Construct the given BST
+        Node root = new Node(8);
+        root.left = new Node(5);
+        root.right = new Node(11);
+        root.left.left = new Node(3);
+        root.left.right = new Node(6);
+        root.right.right = new Node(20);
+        int close = difference(root,10);
+        System.out.print(close);
+	}
+}
