8999aa29c790e13cf3166f26ceeb50d96e2abd41
Riya Kumari <riyamuskan12345@gmail.com>
2026-08-03 19:25:07 +0530

    Add files via upload

---
+++ Binary Trees/mirrorInverted.java
@@ -0,0 +1,36 @@
+package apnaCollege;
+import apnaCollege.diameter2OfNodes.Node;
+public class mirrorInverted
+{
+	public static void mirror(Node root)
+	{
+		if(root == null)
+			return;
+		mirror(root.left);
+		mirror(root.right);
+		
+		Node temp = root.left;
+		root.left = root.right;
+		root.right = temp;
+	}
+	public static void inOrder(Node root) {
+		if(root == null)
+			return;
+		inOrder(root.left);
+		System.out.print(root.data + " ");
+		inOrder(root.right);
+	}
+	public static void main(String[] args) {
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.right.left = new Node(6);
+		root.right.right = new Node(7);
+		inOrder(root);
+		mirror(root);
+		System.out.println();
+		inOrder(root);
+	}
+}
